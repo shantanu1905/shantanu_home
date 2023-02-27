6cc73283ff69d27fc79d7e88edfd43b1cc7c933e
shantanu1905 <nimkarshantanu07@gmail.com>
2023-02-28 00:37:29 +0530

    update 2

--- configurations/settings.py
+++ configurations/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+5ns8c33sq&@1-usg$trofy*n1wfm8m#!3s76tfswg4gas8@z$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shantanu-home.vercel.app','*']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
